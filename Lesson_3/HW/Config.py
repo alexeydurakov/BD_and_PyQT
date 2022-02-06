41ddb96d3c495e2dad0133d7e2e345654c598069
alexeydurakov <alexey.durakov@yandex.ru>
2022-02-06 20:59:38 +0300

    lesson_3

---
+++ Lesson_3/HW/Config.py
@@ -0,0 +1,25 @@
+
+STATUS_RESPONCE = 200
+CONTENT = "application/json"
+ACTION = "presense"
+UNIX_TIME = 1
+USER = 'user'
+ACCOUNT = "account_name"
+PASSWORD = "password"
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
+TIME = 'time'
+ACCOUNT_NAME = 'account_name'
+SENDER = 'from'
+DESTINATION = 'to'
+EXIT = 'exit'
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+RESPONSE_200 = {RESPONSE: 200}
+RESPONSE_400 = {RESPONSE: 400}
+DEFAULT_PORT = 7777
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+MAX_CONNECTIONS = 5
+MAX_PACKAGE_LENGTH = 1024
+ENCODING = 'utf-8'
